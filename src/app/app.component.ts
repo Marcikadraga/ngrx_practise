0e2bc2d8060f9f724f5ecfa007983c15ff0cd1cc
marton.balogh.attila <marton.balogh.attila@gmail.com>
2026-03-02 16:21:18 +0100

    first upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
+import * as PostActions from "./actions/post.actions";
+
+
+interface AppState {
+  post: Post;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,5 +16,25 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ngrxFire';
+  post: Observable<Post>
+  text: string="";
+
+  constructor(private store: Store<AppState>) {
+    this.post = this.store.select('post');
+  }
+  editText() {
+    this.store.dispatch(new PostActions.EditText(this.text)); 
+  }
+
+  resetPost() {
+    this.store.dispatch(new PostActions.Reset());
+  }
+
+  upvote() {
+    this.store.dispatch(new PostActions.Upvote());
+  }
+
+  downvote() {
+    this.store.dispatch(new PostActions.Downvote());
+  }
 }
---
+++ src/app/simple.reducer.ts
@@ -0,0 +1,15 @@
+import { Action } from '@ngrx/store';
+
+export function simpleReducer(state: string = 'Helloo world', action:Action){
+    console.log(action.type,state);
+    
+    switch (action.type) {
+        case 'SPANISH':
+            return 'Hola mundo';
+        case 'FRENCH':
+            return 'Bonjour le monde';
+        default:
+            return state;   
+    }
+    
+}
