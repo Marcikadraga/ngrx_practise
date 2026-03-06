2f7ae549dda8d0f829d9d96935107c60ec9b3af1
Marcikadraga <marciprog@gmail.com>
2026-03-06 14:46:22 +0100

    add login, register feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { LoginPageComponent } from './features/auth/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
+import { HomePageComponent } from './features/home/pages/home-page/home-page.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
+  { path: 'home', component: HomePageComponent },
+];
 
 @NgModule({
-
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,22 @@ import { StoreModule } from '@ngrx/store';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { authReducer } from './features/auth/store/auth.reducer';
+import { HomePageComponent } from './features/home/pages/home-page/home-page.component';
+import { RegisterPageComponent } from './features/auth/pages/register-page/register-page.component';
+import { LoginPageComponent } from './features/auth/pages/login-page/login-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
+    AppRoutingModule,
     StoreModule.forRoot({
       auth: authReducer
     })
@@ -23,3 +30,5 @@ import { authReducer } from './features/auth/store/auth.reducer';
   bootstrap: [AppComponent]
 })
 export class AppModule {}
+
+
